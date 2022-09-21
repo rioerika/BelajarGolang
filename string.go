94ebbf982df15a6db0163168d2d67707beeb3ca5
Rio-Erika <rio_173040041@mail.unpas.ac.id>
2022-09-22 04:13:55 +0700

    input semua file

---
+++ string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println(len("rio"))
+	fmt.Println("Rio Erika"[0])
+	fmt.Print("Rio Erika Saputra"[2])
+}
---
+++ variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string
+	name = " Rio Erika Saputra"
+	fmt.Println(name)
+
+	name = " Rio Erika Saputra ST"
+	fmt.Println(name)
+}
